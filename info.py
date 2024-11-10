44e37264bc93ec731d62db7c7bd1624f817c7298
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2024-11-10 10:01:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,8 +67,8 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'kingvj01') # owner username without @
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/hubrtv_bot')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/hubrtv_bot')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Thalaiva_Group')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Thalaiva_Moviez')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'hubrtv_bot') # Support Chat Link Without https:// or @
 
