3f8152c7d926bf3c8069c951fa21a480f17d3ff9
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2024-11-10 09:36:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '24935727'))
-API_HASH = environ.get('API_HASH', '3fd33336629324ecd664e9b6894f0909)
+API_HASH = environ.get('API_HASH', '3fd33336629324ecd664e9b6894f0909')
 BOT_TOKEN = environ.get('BOT_TOKEN', "7637293116:AAHLBX2hu0aSiwEUNd_wMz20VPc6nkkPWE4")
 
 # Bot settings
@@ -32,7 +32,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002334577561'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7348205141 1087968824').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002177184159').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1798604396').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
